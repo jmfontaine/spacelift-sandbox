28e6f128983c7fcf4591b36aa55a3f3bf728b542
Jean-Marc Fontaine <jmf@spacelift.io>
2022-05-04 18:13:39 -0400

    Update main.tf

--- dummy2/main.tf
+++ dummy2/main.tf
@@ -12,3 +12,18 @@ resource "random_uuid" "test4" {
 
 resource "random_uuid" "test5" {
 }
+
+resource "random_uuid" "test6" {
+}
+
+resource "null_resource" "previous" {}
+
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.previous]
+
+  create_duration = "30s"
+}
+
+resource "null_resource" "next" {
+  depends_on = [time_sleep.wait_30_seconds]
+}
